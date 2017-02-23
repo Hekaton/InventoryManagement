6a127ca1642edda6783b450888f96db79b06e82e
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2017-02-22 22:55:52 -0500

    fix typo

--- src/InventoryManagement/Item/models.py
+++ src/InventoryManagement/Item/models.py
@@ -5,11 +5,4 @@ from django.db import models
 
 # Model of an Item
 class Item(models.Model):
-    item_name = models.CharField(max_lenght=100)
-    objects = ItemManager()
-
-
-class ItemManager(models.Manager):
-
-    def create_item(self, item_name):
-        item = self.create(item_name=item_name)
+    item_name = models.CharField(max_length=100, blank=False, null=False)
